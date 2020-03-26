bcd23aa9f1bb7e7920f70e1179dc5e8eaf0509ad
sankadayananda <62587269+sankadayananda@users.noreply.github.com>
2020-03-26 16:32:28 +0530

    Update datain.sql

--- datain.sql
+++ datain.sql
@@ -1,3 +1,11 @@
+CREATE TABLE family_members (
+  id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  member_name CHAR(100),
+  dob VARCHAR(11),
+  details VARCHAR(4000)
+);
+
+
 INSERT INTO family_members (member_name, dob, details) VALUES ('AAAA DDDD', '2001/10/02', 'Father');
 INSERT INTO family_members (member_name, dob, details) VALUES ('NNNN PPPP', '2002/02/04', 'Mother');
 INSERT INTO family_members (member_name, dob, details) VALUES ('DDDD DDDD', '2003/08/01', 'Son');
