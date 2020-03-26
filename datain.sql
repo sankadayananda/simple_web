31c3440979ef01463b50907a007ebd93d5735bd1
sankadayananda <62587269+sankadayananda@users.noreply.github.com>
2020-03-26 12:14:54 +0530

    Delete datain.sql

--- datain.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO family_members (member_name, dob, details) VALUES ('AAAA DDDD', '1982/10/02', 'Father');
-INSERT INTO family_members (member_name, dob, details) VALUES ('NNNN PPPP', '1988/02/04', 'Mother');
-INSERT INTO family_members (member_name, dob, details) VALUES ('DDDD DDDD', '2017/08/01', 'Son');
-INSERT INTO family_members (member_name, dob, details) VALUES ('SSSS DDDD', '2019/07/04', 'Daughter');
