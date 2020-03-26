25eca9051cbc301c186ac2d44d45564b4757ea22
sankadayananda <62587269+sankadayananda@users.noreply.github.com>
2020-03-26 12:15:12 +0530

    Add files via upload

---
+++ datain.sql
@@ -0,0 +1,4 @@
+INSERT INTO family_members (member_name, dob, details) VALUES ('AAAA DDDD', '2001/10/02', 'Father');
+INSERT INTO family_members (member_name, dob, details) VALUES ('NNNN PPPP', '2002/02/04', 'Mother');
+INSERT INTO family_members (member_name, dob, details) VALUES ('DDDD DDDD', '2003/08/01', 'Son');
+INSERT INTO family_members (member_name, dob, details) VALUES ('SSSS DDDD', '2004/07/04', 'Daughter');
